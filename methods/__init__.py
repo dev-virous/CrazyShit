dcc369f1b5f510ce9108ec57d2180fed6e1304e9
mhmd <ytheroku64@gmail.com>
2024-06-27 16:53:39 +0300

    Add files via upload

--- methods/__init__.py
+++ methods/__init__.py
@@ -1,1 +1,10 @@
+from .send_message import SendMessage
+from .get_last_message import GetLastMessage
+from .esc import Escap
 
+class Methods(
+    SendMessage,
+    GetLastMessage,
+    Escap
+):
+    pass
